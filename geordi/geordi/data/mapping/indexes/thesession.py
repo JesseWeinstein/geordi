a29f20d425d45613117357bc764b39a2c9957410
Jesse Weinstein <jesse@wefu.org>
2014-12-07 01:01:41 -0800

    Add rules for tracks, artists, and stick the name in release_group, too

--- geordi/geordi/data/mapping/indexes/thesession.py
+++ geordi/geordi/data/mapping/indexes/thesession.py
@@ -1,5 +1,11 @@
 from ..rule import Rule
 
 thesession = {
-    'release': [Rule(['recording'], ['release', 'name'])]
+    'release': [Rule(['recording'], ['release_group', 'name']),
+                Rule(['recording'], ['release', 'name']),
+                Rule(['tracks', ('t_index', True)],
+                     lambda *args, **kwargs: ['release', 'mediums', 'split', 'tracks', (kwargs.get('t_index'),), 'name'],
+                     transform=lambda val, *args, **kwargs: ' / '.join(v for (k,v) in sorted(val.items()))),
+                Rule(['artist'], ['release', 'artists', 'unsplit'])
+                ]
 }
